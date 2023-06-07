fa1f5e4ae76580db07eeecc51a025114ae897dcd
Bradley Hagger <brad_2nd@hotmail.com>
2023-06-07 13:03:06 +0100

    Added useEffect hook to call api and set the initial state of quotes. Updated rendered JSX with a p to contain quote text, span to contain author name and button to display a new quote. Added a random number function to generate a random quote and a validate quote function to validate that the quote has both an author and text to be displayed.

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -2,33 +2,67 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
-const quotes = () => {
+const Quotes = () => {
   const [quotes, setQuotes] = useState([])
+  const [randomNumber, setRandomNumber] = useState(0)
 
   useEffect(() => {
-    const retrieveQuotes = async () => {
+    const fetchFiftyQuotes = async () => {
       const response = await fetch("https://type.fit/api/quotes")
       const data = await response.json()
       setQuotes(data)
-    }
+      console.log(data)
+      console.log(quotes, "quotes")
+    } 
 
-    retrieveQuotes()
-
+    fetchFiftyQuotes()
   }, [])
 
-  console.log(quotes[0], "quotes")
+  // Return text from random quote
+  const updateQuoteText = (randomNumber) => {
+    console.log(randomNumber)
+    if (quotes.length > 0 && quotes[randomNumber].text) {
+      validateQuote(randomNumber)
+      return quotes[randomNumber].text
+    }
+    return "";
+  };
 
+  // Return author from random quote
+  const updateAuthorText = (randomNumber) => {
+    console.log(randomNumber)
+    if (quotes.length > 0 && quotes[randomNumber].author) {
+      validateQuote(randomNumber)
+      return quotes[randomNumber].author
+    }
+    return "";
+  }
+
+  // Validate if quote has both text and an author, if not then generate a new quote
+  const validateQuote = (randomNumber) => {
+    if(quotes[randomNumber].text != undefined && quotes[randomNumber].author != undefined){
+      return
+    }
+    generateRandomNumber()
+  }
+
+  // Helper to generate random number and assign to randomNumber state
+  const generateRandomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * quotes.length)
+    setRandomNumber(randomNumber)
+  }
+  
   return (
     <div>
-      <p></p>
-      <span></span>
+      <p>{updateQuoteText(randomNumber)}</p>
+      <span>{updateAuthorText(randomNumber)}</span>
       <div>
-        <button></button>
-        <button></button>
-        <button>New Quote</button>
+        <button onClick={generateRandomNumber}>New Quote</button>
       </div>
     </div>
   )
 }
 
-export default quotes
+export default Quotes
+
+  
